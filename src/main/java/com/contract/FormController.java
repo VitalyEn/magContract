8899bbf81820e4cfb6b6d8d70c940bb9f26ba9d2
Vitaly Engel <vitaliy.engel@gmail.com>
2022-05-18 16:52:27 +0300

    Загрузка данных - тестовая загрузка строки excel

--- src/main/java/com/contract/FormController.java
+++ src/main/java/com/contract/FormController.java
@@ -97,26 +97,21 @@ public class FormController implements Initializable {
         tableFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         tableSecondName.setCellValueFactory(new PropertyValueFactory<>("secondName"));
         tableFathersName.setCellValueFactory(new PropertyValueFactory<>("fathersName"));
-        tableId.setCellValueFactory(new PropertyValueFactory<>("id"));
-       /* private Integer id;
-        private String firstName;
-        private String secondName;
-        private String fathersName;
-        private String birthDate;
-        private String birthPlace;
-        private String flat;
-        private String square;
-        private String sertNumber;
-        private String regDate;
-        private String passportSerie;
-        private String passportNumber;
-        private String passportOutput;
-        private String passportCode;
-        private String regAdress;
-        private String liveAdress;
-        private String phone;
-        private String mail;
-        private String gasService;*/
+        tableBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        tableBirthPlace.setCellValueFactory(new PropertyValueFactory<>("birthPlace"));
+        tableFlat.setCellValueFactory(new PropertyValueFactory<>("flat"));
+        tableSquare.setCellValueFactory(new PropertyValueFactory<>("square"));
+        tableSertNumber.setCellValueFactory(new PropertyValueFactory<>("sertNumber"));
+        tableRegDate.setCellValueFactory(new PropertyValueFactory<>("regDate"));
+        tablePassportSerie.setCellValueFactory(new PropertyValueFactory<>("passportSerie"));
+        tablePassportNumber.setCellValueFactory(new PropertyValueFactory<>("passportNumber"));
+        tablePassportOutput.setCellValueFactory(new PropertyValueFactory<>("passportOutput"));
+        tablePassportCode.setCellValueFactory(new PropertyValueFactory<>("passportCode"));
+        tableRegAdress.setCellValueFactory(new PropertyValueFactory<>("regAdress"));
+        tableLiveAdress.setCellValueFactory(new PropertyValueFactory<>("liveAdress"));
+        tablePhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        tableMail.setCellValueFactory(new PropertyValueFactory<>("mail"));
+        tableGasService.setCellValueFactory(new PropertyValueFactory<>("gasService"));
     }
 
     @FXML //Обработка кнопки "Сохранить результат"
@@ -137,8 +132,9 @@ public class FormController implements Initializable {
     @FXML
     private void tableLoad() {
         // Открытие нового докусента Exel (только первый лист!)
+        if (tableTemplate.getItems() != null) tableTemplate.getItems().clear();
         ExcelDocument exel = new ExcelDocument();
-        dataTemplate = exel.readRow(1);
+        dataTemplate = exel.readRow(0);
         this.id = 1;
         this.firstName = dataTemplate.get(0);
         this.secondName = dataTemplate.get(1);
@@ -153,11 +149,11 @@ public class FormController implements Initializable {
         this.passportNumber = dataTemplate.get(10);
         this.passportOutput = dataTemplate.get(11);
         this.passportCode = dataTemplate.get(12);
-        //this.regAdress = dataTemplate.get(14);
-        //this.liveAdress = dataTemplate.get(15);
-        //this.phone = dataTemplate.get(16);
-        //this.mail = dataTemplate.get(17);
-        //this.gasService = dataTemplate.get(18);
+        this.regAdress = dataTemplate.get(13);
+        this.liveAdress = dataTemplate.get(14);
+        this.phone = dataTemplate.get(15);
+        this.mail = dataTemplate.get(16);
+        this.gasService = dataTemplate.get(17);
 
         initData();
         tableTemplate.setItems(opData);
